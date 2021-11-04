046b065081a9772b8989dac68251dd3914e8ff90
matheus-miranda <matheus.sm86@gmail.com>
2021-11-04 10:43:04 -0300

    Create LoginFragment.kt and Navigation graph

---
+++ app/src/main/java/com/invillia/meubeats/ui/LoginFragment.kt
@@ -0,0 +1,19 @@
+package com.invillia.meubeats.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.invillia.meubeats.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
