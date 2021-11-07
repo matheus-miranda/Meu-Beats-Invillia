0f4f1bc9fdb4c0d0dd3d7c15e2e4dd96b1895324
matheus-miranda <matheus.sm86@gmail.com>
2021-11-07 17:10:20 -0300

    Create EntityMapper Interface

---
+++ app/src/main/java/com/invillia/meubeats/data/mapper/EntityMapper.kt
@@ -0,0 +1,12 @@
+package com.invillia.meubeats.data.mapper
+
+interface EntityMapper<Entity, DomainModel> {
+
+    fun toDomain(entity: Entity): DomainModel
+
+    fun toEntity(domainModel: DomainModel): Entity
+
+    fun toDomainList(entityList: List<Entity>) = entityList.map { toDomain(it) }
+
+    fun toEntityList(domainList: List<DomainModel>) = domainList.map { toEntity(it) }
+}
