e86ca2097f7ab64a4c8c4b0fe0b7e7cfe06e09bc
matheus-miranda <matheus.sm86@gmail.com>
2021-11-07 14:42:28 -0300

    Create getNetworkHeadphones use case

---
+++ app/src/main/java/com/invillia/meubeats/domain/usecase/getNetworkHeadphonesUseCase.kt
@@ -0,0 +1,11 @@
+package com.invillia.meubeats.domain.usecase
+
+import com.invillia.meubeats.domain.model.Headphone
+import com.invillia.meubeats.domain.repository.BeatsRepository
+import kotlinx.coroutines.flow.Flow
+
+class getNetworkHeadphonesUseCase(private val repository: BeatsRepository) {
+    operator fun invoke(): Flow<List<Headphone>> {
+        return repository.getNetworkHeadphones()
+    }
+}
