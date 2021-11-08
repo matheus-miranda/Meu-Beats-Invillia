07d65f491a74602bb52bbb9993aa6544f8ca77bb
matheus-miranda <matheus.sm86@gmail.com>
2021-11-08 15:55:10 -0300

    Create recycler view adapter class ProductListAdapter, add Glide dependency

---
+++ app/src/main/java/com/invillia/meubeats/presentation/adapter/ProductListAdapter.kt
@@ -0,0 +1,46 @@
+package com.invillia.meubeats.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.invillia.meubeats.databinding.ProductListItemBinding
+import com.invillia.meubeats.domain.model.Headphone
+
+class ProductListAdapter :
+    ListAdapter<Headphone, ProductListAdapter.ProductViewHolder>(DiffCallBack()) {
+
+    inner class ProductViewHolder(private val binding: ProductListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Headphone) {
+            binding.apply {
+                tvPhoneModel.text = item.model
+                chipRating.text = item.rating.toString()
+                tvNumberReviews.text = item.totalReviews.toString()
+                tvPrice.text = item.price.toString()
+                Glide.with(root.context).load(item.image).into(ivPhoto)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ProductViewHolder(ProductListItemBinding.inflate(inflater, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+class DiffCallBack : DiffUtil.ItemCallback<Headphone>() {
+    override fun areItemsTheSame(oldItem: Headphone, newItem: Headphone) =
+        oldItem.model == newItem.model
+
+    override fun areContentsTheSame(oldItem: Headphone, newItem: Headphone) =
+        oldItem == newItem
+}
