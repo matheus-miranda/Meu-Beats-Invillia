9ffa005c0f557833a47de9255ded072a9a8c40f4
matheus-miranda <matheus.sm86@gmail.com>
2021-11-08 10:43:35 -0300

    Display list of headphones from network on ProductListFragment and add Timber

--- app/src/main/java/com/invillia/meubeats/BeatsApplication.kt
+++ app/src/main/java/com/invillia/meubeats/BeatsApplication.kt
@@ -4,12 +4,14 @@ import android.app.Application
 import com.invillia.meubeats.di.AppModules
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 class BeatsApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         startKoin()
+        Timber.plant(Timber.DebugTree())
     }
 
     private fun startKoin() {
--- app/src/main/java/com/invillia/meubeats/presentation/ui/ProductListFragment.kt
+++ app/src/main/java/com/invillia/meubeats/presentation/ui/ProductListFragment.kt
@@ -5,13 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.invillia.meubeats.R
 import com.invillia.meubeats.databinding.FragmentProductListBinding
+import com.invillia.meubeats.domain.model.Headphone
+import com.invillia.meubeats.presentation.viewmodel.ProductListViewModel
+import com.invillia.meubeats.presentation.viewmodel.State
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import timber.log.Timber
 
 class ProductListFragment : Fragment() {
 
     private var _binding: FragmentProductListBinding? = null
     private val binding get() = _binding!!
+    private val viewModel by viewModel<ProductListViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,10 +36,44 @@ class ProductListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         bindToolbar()
+        bindObserver()
     }
 
     private fun bindToolbar() {
         binding.tbProductList.inflateMenu(R.menu.menu_product_list)
+    }
+
+    private fun bindObserver() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.state.collect(::getHeadphones)
+            }
+        }
+    }
+
+    private fun getHeadphones(state: State) {
+        when (state) {
+            is State.Loading -> showLoading()
+            is State.Error -> showError(state.throwable)
+            is State.EmptyList -> showEmptyList()
+            is State.Success -> showSuccess(state.headphoneList)
+        }
+    }
+
+    private fun showLoading() {
+        Timber.d("Loading")
+    }
+
+    private fun showError(throwable: Throwable) {
+        Timber.d("Error: ${throwable.message}")
+    }
+
+    private fun showEmptyList() {
+        Timber.d("Empty")
+    }
+
+    private fun showSuccess(headphoneList: List<Headphone>) {
+        Timber.d("Success: $headphoneList")
     }
 
     override fun onDestroyView() {
