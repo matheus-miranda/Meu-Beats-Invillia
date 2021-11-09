04ed9f6c7eab7574f078a02f19edc4756f3c9594
matheus-miranda <matheus.sm86@gmail.com>
2021-11-09 14:29:16 -0300

    Display data from Product list and add UP navigation behavior

--- app/src/main/java/com/invillia/meubeats/presentation/ui/ProductDetailsFragment.kt
+++ app/src/main/java/com/invillia/meubeats/presentation/ui/ProductDetailsFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.invillia.meubeats.databinding.FragmentProductDetailsBinding
 
 class ProductDetailsFragment : Fragment() {
@@ -24,7 +26,28 @@ class ProductDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        bindToolbar()
+        bindData()
+    }
+
+    private fun bindToolbar() {
+        binding.tbProductDetails.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun bindData() {
         val receivedHeadphone = args.headphone
+        with(binding) {
+            tvModelName.text = receivedHeadphone.model
+            tvConnection.text = receivedHeadphone.connection
+            tvCompatibility.text = receivedHeadphone.compatibility
+            tvCharging.text = receivedHeadphone.charge
+            tvAutonomy.text = receivedHeadphone.autonomy
+            tvHeight.text = receivedHeadphone.height
+            tvCapture.text = receivedHeadphone.capture
+            Glide.with(root.context).load(receivedHeadphone.image).into(ivHeadphone)
+        }
     }
 
     override fun onDestroyView() {
