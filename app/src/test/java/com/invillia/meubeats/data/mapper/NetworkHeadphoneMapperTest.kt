c46d77a56aa7ba8f8f43db3815d7fce30849c3fd
matheus-miranda <matheus.sm86@gmail.com>
2021-11-10 11:25:27 -0300

    Create tests for the Mapper functions

---
+++ app/src/test/java/com/invillia/meubeats/data/mapper/NetworkHeadphoneMapperTest.kt
@@ -0,0 +1,39 @@
+package com.invillia.meubeats.data.mapper
+
+import com.google.common.truth.Truth.assertThat
+import com.invillia.meubeats.util.TestData.HEADPHONE
+import com.invillia.meubeats.util.TestData.NETWORK_HEADPHONE
+import org.junit.Test
+
+class NetworkHeadphoneMapperTest {
+
+    private val mapper = NetworkHeadphoneMapper()
+
+    @Test
+    fun toDomain() {
+        val expectedResult = HEADPHONE
+        val returnedHeadphone = mapper.toDomain(NETWORK_HEADPHONE)
+        assertThat(returnedHeadphone).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun toEntity() {
+        val expectedResult = NETWORK_HEADPHONE
+        val returnedHeadphone = mapper.toEntity(HEADPHONE)
+        assertThat(returnedHeadphone).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun toDomainList() {
+        val expectedResult = listOf(HEADPHONE)
+        val returnedHeadphoneList = mapper.toDomainList(listOf(NETWORK_HEADPHONE))
+        assertThat(returnedHeadphoneList).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun toEntityList() {
+        val expectedResult = listOf(NETWORK_HEADPHONE)
+        val returnedHeadphoneList = mapper.toEntityList(listOf(HEADPHONE))
+        assertThat(returnedHeadphoneList).isEqualTo(expectedResult)
+    }
+}
--- app/src/test/java/com/invillia/meubeats/util/TestData.kt
+++ app/src/test/java/com/invillia/meubeats/util/TestData.kt
@@ -1,11 +1,26 @@
 package com.invillia.meubeats.util
 
+import com.invillia.meubeats.data.remote.dto.NetworkHeadphone
 import com.invillia.meubeats.domain.model.Headphone
 import io.mockk.MockKAnnotations
 
 object TestData {
 
     val HEADPHONE = Headphone(
+        model = "model",
+        rating = 1.1,
+        price = 2.99,
+        totalReviews = 10,
+        image = "image.jpg",
+        connection = "connection",
+        compatibility = "compatibility",
+        charge = "charge",
+        autonomy = "autonomy",
+        height = "height",
+        capture = "capture"
+    )
+
+    val NETWORK_HEADPHONE = NetworkHeadphone(
         model = "model",
         rating = 1.1,
         price = 2.99,
