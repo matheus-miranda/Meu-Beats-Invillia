fd3930a1c0be6a8e67e1872394607ad31614680c
matheus-miranda <matheus.sm86@gmail.com>
2021-11-10 10:46:00 -0300

    Refactor test

--- app/src/test/java/com/invillia/meubeats/domain/usecase/GetNetworkHeadphonesUseCaseTest.kt
+++ app/src/test/java/com/invillia/meubeats/domain/usecase/GetNetworkHeadphonesUseCaseTest.kt
@@ -6,22 +6,22 @@ import com.invillia.meubeats.domain.repository.BeatsRepository
 import com.invillia.meubeats.util.TestData.initMockkAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
-import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.impl.annotations.MockK
 import io.mockk.verify
 import kotlinx.coroutines.flow.flow
+import org.junit.Before
 import org.junit.Test
 
 class GetNetworkHeadphonesUseCaseTest {
 
-    @RelaxedMockK
+    @MockK
     private lateinit var repository: BeatsRepository
 
     @InjectMockKs
     private lateinit var getNetworkHeadphonesUseCase: GetNetworkHeadphonesUseCase
 
-    init {
-        initMockkAnnotations()
-    }
+    @Before
+    fun setup() = initMockkAnnotations()
 
     @Test
     fun `UseCase emits a Flow of Headphone objects when Repository is called once`() {
