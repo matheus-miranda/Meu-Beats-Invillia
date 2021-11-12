83245c77b2c85f02b9be86671eabeebdabf9cb13
Matheus Miranda <15269393+matheus-miranda@users.noreply.github.com>
2021-11-12 09:28:58 -0300

    refactor/improvements (#1)
    * Extracts styles for future layout modifications
    * Replace data class with regular class

--- app/src/main/java/com/invillia/meubeats/domain/model/Headphone.kt
+++ app/src/main/java/com/invillia/meubeats/domain/model/Headphone.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class Headphone(
+class Headphone(
     val model: String,
     val rating: Double,
     val price: Double,
@@ -16,4 +16,42 @@ data class Headphone(
     val autonomy: String,
     val height: String,
     val capture: String
-) : Parcelable
+) : Parcelable {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Headphone
+
+        if (model != other.model) return false
+        if (rating != other.rating) return false
+        if (price != other.price) return false
+        if (totalReviews != other.totalReviews) return false
+        if (image != other.image) return false
+        if (connection != other.connection) return false
+        if (compatibility != other.compatibility) return false
+        if (charge != other.charge) return false
+        if (autonomy != other.autonomy) return false
+        if (height != other.height) return false
+        if (capture != other.capture) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = model.hashCode()
+        result = 31 * result + rating.hashCode()
+        result = 31 * result + price.hashCode()
+        result = 31 * result + totalReviews
+        result = 31 * result + image.hashCode()
+        result = 31 * result + connection.hashCode()
+        result = 31 * result + compatibility.hashCode()
+        result = 31 * result + charge.hashCode()
+        result = 31 * result + autonomy.hashCode()
+        result = 31 * result + height.hashCode()
+        result = 31 * result + capture.hashCode()
+        return result
+    }
+
+}
