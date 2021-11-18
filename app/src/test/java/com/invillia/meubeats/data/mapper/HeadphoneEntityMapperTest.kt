9d66c747450811e69ee7f39960eba5f1ea9f59fe
matheus-miranda <matheus.sm86@gmail.com>
2021-11-18 12:03:30 -0300

    Create test for HeadphoneEntityMapper

---
+++ app/src/test/java/com/invillia/meubeats/data/mapper/HeadphoneEntityMapperTest.kt
@@ -0,0 +1,39 @@
+package com.invillia.meubeats.data.mapper
+
+import com.google.common.truth.Truth.assertThat
+import com.invillia.meubeats.util.TestData.ENTITY_HEADPHONE
+import com.invillia.meubeats.util.TestData.HEADPHONE
+import org.junit.Test
+
+class HeadphoneEntityMapperTest {
+
+    private val mapper = HeadphoneEntityMapper()
+
+    @Test
+    fun toDomain() {
+        val expectedResult = HEADPHONE
+        val returnedResult = mapper.toDomain(ENTITY_HEADPHONE)
+        assertThat(returnedResult).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun toEntity() {
+        val expectedResult = ENTITY_HEADPHONE
+        val returnedResult = mapper.toEntity(HEADPHONE)
+        assertThat(returnedResult).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun toDomainList() {
+        val expectedResult = listOf(HEADPHONE)
+        val returnedResult = mapper.toDomainList(listOf(ENTITY_HEADPHONE))
+        assertThat(returnedResult).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun toEntityList() {
+        val expectedResult = listOf(ENTITY_HEADPHONE)
+        val returnedResult = mapper.toEntityList(listOf(HEADPHONE))
+        assertThat(returnedResult).isEqualTo(expectedResult)
+    }
+}
